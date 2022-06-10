2b62018b1ed1a639334a67cad519e84aa3609df8
LuoChen <luochen1990@gmail.com>
2022-06-10 13:55:28 +0800

    improve: EasyMode.Error

--- src/EasyMode/Error.hs
+++ src/EasyMode/Error.hs
@@ -18,13 +18,13 @@ import Data.Bool (Bool)
 import Data.List ((++))
 
 blame :: forall (r :: RuntimeRep). forall (a :: TYPE r). HasCallStack => Text -> a
-blame msg = E.errorWithoutStackTrace ("Improper Call: " ++ unpack msg)
+blame msg = E.errorWithoutStackTrace ("[Improper Call]: " ++ unpack msg)
 
 error :: forall (r :: RuntimeRep). forall (a :: TYPE r). Text -> a
-error msg = E.errorWithoutStackTrace ("Internal Error: " ++ unpack msg)
+error msg = E.errorWithoutStackTrace ("[Internal Error]: " ++ unpack msg)
 
 assume :: HasCallStack => Bool -> a -> a
-assume c r = if c then r else E.errorWithoutStackTrace "Improper Call: assumption failed"
+assume c r = if c then r else E.errorWithoutStackTrace "[Improper Call]: assumption failed"
 
 assert :: Bool -> a -> a
-assert c r = if c then r else E.errorWithoutStackTrace "Internal Error: assertion failed"
+assert c r = if c then r else E.errorWithoutStackTrace "[Internal Error]: assertion failed"
