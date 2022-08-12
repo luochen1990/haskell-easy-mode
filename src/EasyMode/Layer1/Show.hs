44bbd71d00b6e5d50385751da423d3cc6b14b183
LuoChen <luochen1990@gmail.com>
2022-08-12 22:11:13 +0800

    add: instance of Show

--- src/EasyMode/Layer1/Show.hs
+++ src/EasyMode/Layer1/Show.hs
@@ -2,12 +2,17 @@
 
 module EasyMode.Layer1.Show where
 
-import Data.Text as Export (Text, pack, unpack)
+import Data.Text (Text, pack, unpack)
 import qualified Prelude as P
 
 class Show a where
     show :: a -> Text
 
-instance P.Show a => Show a where
+-- {-# OVERLAPS / OVERLAPPING / OVERLAPPABLE #-}
+
+instance {-# OVERLAPS #-} P.Show a => Show a where
     show x = pack (P.show x)
+
+instance {-# OVERLAPS #-} Show a => P.Show a where
+    show x = unpack (show x)
 
