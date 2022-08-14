3b54e25153f1f7b5afd4810e67c109111f72995a
LuoChen <luochen1990@gmail.com>
2022-08-15 00:53:26 +0800

    fix: Show

--- src/EasyMode/Show.hs
+++ src/EasyMode/Show.hs
@@ -1,41 +1,25 @@
 {-# language UndecidableInstances #-}
 
 module EasyMode.Show (
-    Show(..)
+    show
 ) where
 
 import Data.Text (Text, pack, unpack)
 import qualified Prelude as P
 import qualified GHC.Real as P ((%), Ratio((:%)))
-import qualified TextShow as T
 import Data.Function ((&))
-import Data.Text.Lazy as T (toStrict)
-import TextShow (TextShow)
+-- import qualified TextShow as T
+-- import Data.Text.Lazy as T (toStrict)
+-- import TextShow (TextShow)
 
-class Show a where
-    show :: a -> Text
-    show x = showbPrec 0 x & T.toLazyText & T.toStrict
-
-    showbPrec :: P.Integer -> a -> T.Builder
-    showbPrec _ x = T.fromText (show x)
+show :: P.Show a => a -> Text
+show x = pack (P.show x)
 
 
--- {-# OVERLAPS / OVERLAPPING / OVERLAPPABLE #-}
-
-instance {-# OVERLAPPABLE #-} P.Show a => Show a where
-    showbPrec p = T.showsToShowb (P.showsPrec (P.fromIntegral p))
-
--- instance {-# OVERLAPS #-} Show a => P.Show a where
---     show x = unpack (show x)
-
--- instance {-# OVERLAPPING #-} TextShow a => Show a where
---     show x = showt x
-
-instance {-# OVERLAPPING #-} (P.Show a)  => Show (P.Ratio a)  where
-    {-# SPECIALIZE instance Show P.Rational #-}
-    showbPrec p = T.showsToShowb (showsPrec (P.fromIntegral p)) where
-        showsPrec p (x P.:% y)  =  P.showParen (p P.> 7) P.$
-                                   P.showsPrec 8 x P..
-                                   P.showString " / " P..
-                                   P.showsPrec 8 y
+instance {-# OVERLAPPING #-} P.Show P.Rational  where
+    {-# SPECIALIZE instance P.Show P.Rational #-}
+    showsPrec p (x P.:% y)  =  P.showParen (p P.> 7) P.$
+                               P.showsPrec 8 x P..
+                               P.showString " / " P..
+                               P.showsPrec 8 y
 
