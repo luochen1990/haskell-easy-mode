0c8ad3f8ae7d8c00d474c3c49915a220b5afd1bf
LuoChen <luochen1990@gmail.com>
2022-10-07 13:53:38 +0800

    redesign: [Shell] shellEval returns errCode

--- src/EasyMode/Shell.hs
+++ src/EasyMode/Shell.hs
@@ -9,9 +9,9 @@ import EasyMode.Layers.L1
 import GHC.IO.Exception (ExitCode (..))
 import System.Process (proc, readCreateProcessWithExitCode, shell, waitForProcess, withCreateProcess)
 
-shellEval :: Partial => Text -> IO (Either Text Text)
+shellEval :: Partial => Text -> IO (Either (Int64, Text) Text)
 shellEval cmd = do
     (code, out, err) <- readCreateProcessWithExitCode (shell (unpack cmd)) ""
     case code of
         ExitSuccess -> return (Right (pack out))
-        ExitFailure errorCode -> return (Left (pack err))
+        ExitFailure errCode -> return (Left (errCode, (pack err)))
