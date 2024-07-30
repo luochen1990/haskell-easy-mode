9751f1aba6ba44959da1a1bef748a9dbfc01599f
LuoChen <luochen1990@gmail.com>
2024-07-30 11:50:45 +0800

    rename: Cast -> CastTo

--- src/EasyMode/Cast.hs
+++ src/EasyMode/Cast.hs
@@ -31,13 +31,13 @@ import Prelude (Rational, all, const, either, even, maybe, reads, realToFrac, re
 
     forall a b: Type, x: a.  (pcast (cast x :: b) :: a) == x
 -}
-class Cast target source where
+class CastTo target source where
     cast :: source -> target
 
 {- |
  minimal impl: ecast | mcast
 -}
-class PartialCast target source where
+class PartialCastTo target source where
     -- | opinionated partial cast
     pcast :: Partial => source -> target
     pcast x = case ecast x of
@@ -54,7 +54,7 @@ class PartialCast target source where
 
 -- * reflexive instance
 
-instance Cast a a where cast = id
+instance CastTo a a where cast = id
 
 -- * transitive instances
 
@@ -71,109 +71,109 @@ instance (PartialCast c b, PartialCast b a) => PartialCast c a where
 
 -- * instances for text
 
-instance Cast Text ByteString where cast = decodeUtf8
+instance CastTo Text ByteString where cast = decodeUtf8
 
 -- instance Cast Text (Digest a)   where cast = toHex
 
-instance Cast Text String where cast = pack
+instance CastTo Text String where cast = pack
 
-instance Cast String Text where cast = unpack
+instance CastTo String Text where cast = unpack
 
 -- * instances for number
 
-instance Cast Integer Int where cast = fromIntegral
+instance CastTo Integer Int where cast = fromIntegral
 
-instance PartialCast Int Integer where
+instance PartialCastTo Int Integer where
     ecast x =
         if x <= fromIntegral (maxBound :: Int) && x >= fromIntegral (minBound :: Int)
             then Right (fromIntegral x)
             else Left "cannot cast this Integer to Int since accuracy overflow"
 
-instance PartialCast Integer String where
+instance PartialCastTo Integer String where
     ecast s = case reads s of
         [(x, [])] -> Right x
         [(x, r)] -> Left ("cannot parse this String to Integer, invalid chars: `" ++ pack r ++ "`")
         _ -> impossible
 
-instance PartialCast Float64 String where
+instance PartialCastTo Float64 String where
     ecast s = case reads s of
         [(x, [])] -> Right x
         [(x, r)] -> Left ("cannot parse this String to Float64, invalid chars: `" ++ pack r ++ "`")
         _ -> impossible
 
-instance PartialCast Int String where
+instance PartialCastTo Int String where
     ecast = (ecast :: String -> Either Text Integer) >=> ecast
 
-instance PartialCast Integer Text where mcast s = mcast (unpack s)
+instance PartialCastTo Integer Text where mcast s = mcast (unpack s)
 
-instance PartialCast Float64 Text where mcast s = mcast (unpack s)
+instance PartialCastTo Float64 Text where mcast s = mcast (unpack s)
 
-instance Cast Integer Bool where cast b = if b then 1 else 0
+instance CastTo Integer Bool where cast b = if b then 1 else 0
 
-instance Cast Int Bool where cast b = if b then 1 else 0
+instance CastTo Int Bool where cast b = if b then 1 else 0
 
-instance Cast Float64 Int where cast = fromIntegral
+instance CastTo Float64 Int where cast = fromIntegral
 
-instance Cast Float64 Integer where cast = fromIntegral
+instance CastTo Float64 Integer where cast = fromIntegral
 
-instance Cast Float64 Float32 where cast = realToFrac
+instance CastTo Float64 Float32 where cast = realToFrac
 
-instance Integral a => Cast Float64 (Ratio a) where cast = realToFrac
+instance Integral a => CastTo Float64 (Ratio a) where cast = realToFrac
 
-instance Cast Float32 Int where cast = fromIntegral
+instance CastTo Float32 Int where cast = fromIntegral
 
-instance Cast Float32 Integer where cast = fromIntegral
+instance CastTo Float32 Integer where cast = fromIntegral
 
-instance Cast Float32 Float64 where cast = realToFrac
+instance CastTo Float32 Float64 where cast = realToFrac
 
-instance Integral a => Cast Float32 (Ratio a) where cast = realToFrac
+instance Integral a => CastTo Float32 (Ratio a) where cast = realToFrac
 
 -- * instances for hex
 
-instance Cast ByteString HexString where cast (HexString bs) = bs
+instance CastTo ByteString HexString where cast (HexString bs) = bs
 
-instance Cast HexString ByteString where cast bs = HexString bs
+instance CastTo HexString ByteString where cast bs = HexString bs
 
-instance Cast Text HexString where cast (HexString bs) = decodeLatin1 (convertToBase Base16 bs)
+instance CastTo Text HexString where cast (HexString bs) = decodeLatin1 (convertToBase Base16 bs)
 
-instance PartialCast HexString Text where
+instance PartialCastTo HexString Text where
     ecast txt = either (Left <<< pack) (Right <<< HexString) (decodeHex (encodeUtf8 txt))
       where
         decodeHex :: ByteString -> Either String ByteString
         decodeHex bs = convertFromBase Base16 (if even (BS.length bs) then bs else "0" ++ bs)
 
-instance Cast String HexString where cast hex = unpack (toText hex)
+instance CastTo String HexString where cast hex = unpack (toText hex)
 
-instance Cast Integer HexString where
+instance CastTo Integer HexString where
     cast hs = let [(r, m)] = (readHex (toString hs)) in r -- TODO: optmize
 
-instance PartialCast HexString Integer where
+instance PartialCastTo HexString Integer where
     ecast x = if x >= 0 then Right (pcast (pack (showHex x ""))) else Left "negative Integer cannot be cast to HexString" -- TODO: optmize
 
 -- * instances for maps
 
-instance Cast [(k, v)] (M.HashMap k v) where cast = M.toList
+instance CastTo [(k, v)] (M.HashMap k v) where cast = M.toList
 
-instance Hashable k => Cast (M.HashMap k v) [(k, v)] where cast = M.fromList
+instance Hashable k => CastTo (M.HashMap k v) [(k, v)] where cast = M.fromList
 
 -- * helper functions
 
-toInteger :: PartialCast Integer a => a -> Integer
+toInteger :: PartialCastTo Integer a => a -> Integer
 toInteger = pcast
 
-toFloat :: PartialCast Float64 a => a -> Float64
+toFloat :: PartialCastTo Float64 a => a -> Float64
 toFloat = pcast
 
-toText :: Cast Text a => a -> Text
+toText :: CastTo Text a => a -> Text
 toText = cast
 
-toString :: Cast String a => a -> String
+toString :: CastTo String a => a -> String
 toString = cast
 
-toHex :: Cast HexString a => a -> HexString
+toHex :: CastTo HexString a => a -> HexString
 toHex = cast
 
-toPairs :: Cast [(k, v)] (f k v) => f k v -> [(k, v)]
+toPairs :: CastTo [(k, v)] (f k v) => f k v -> [(k, v)]
 toPairs = cast
 
 toMap :: Hashable k => [(k, v)] -> M.HashMap k v
