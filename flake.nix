3a57399f056cc4595332178904af79d008ef0494
LuoChen <luochen1990@gmail.com>
2024-07-30 20:33:46 +0800

    add: [flake] ghci as flake apps (nix run)

--- flake.nix
+++ flake.nix
@@ -6,22 +6,44 @@
 
   outputs = { self, nixpkgs }:
   let
-    project_name = "easy-mode";
+    project-name = "easy-mode";
+    ghc-version = "ghc98";
     supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
       inherit system;
       pkgs = nixpkgs.legacyPackages.${system};
-      hpkgs = pkgs.haskell.packages.ghc98;
+      hpkgs = pkgs.haskell.packages.${ghc-version};
+      ghc = hpkgs.ghcWithPackages (p: [ self.packages.${system}.default ]);
     });
   in
   rec {
     packages = eachSystem ({hpkgs, ...}: {
-      default = hpkgs.callCabal2nix project_name ./. { };
+      default = hpkgs.callCabal2nix project-name ./. { };
     });
 
     devShells = eachSystem ({pkgs, hpkgs, system, ...}: {
       default = pkgs.haskell.lib.addBuildTools packages.${system}.default
         (with hpkgs; [ haskell-language-server cabal-install ]);
+    });
+
+    apps = eachSystem ({pkgs, ghc, system, ...}: {
+      default = {
+        type = "app";
+        #program = "${packages.${system}.default}/bin/${project_name}";
+        program = "${pkgs.writeShellScript "${project-name}-ghci" ''
+          #export GHC_PACKAGE_PATH=${ "${packages.${system}.default}/" }
+          exec ${ghc}/bin/ghci -ghci-script ${
+            pkgs.writeText ".ghci" ''
+              :set -XOverloadedStrings
+              :set -XOverloadedRecordDot
+              :set -XDuplicateRecordFields
+              :set -XNoFieldSelectors
+              :set -XNoImplicitPrelude
+              :module + EasyMode
+            ''
+          }
+        ''}";
+      };
     });
   };
 }
