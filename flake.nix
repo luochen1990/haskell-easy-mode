f57af26bbe54e27c4063388de0ba60d45e8ce857
LuoChen <luochen1990@gmail.com>
2024-07-30 18:22:36 +0800

    chore: refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,27 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        hpkgs = pkgs.haskell.packages.ghc928;
-      in rec {
-        defaultPackage = hpkgs.callCabal2nix "easy-mode" ./. { };
-        devShell = pkgs.haskell.lib.addBuildTools defaultPackage
-          (with hpkgs; [ haskell-language-server cabal-install hpack hoogle ]);
-      });
+  outputs = { self, nixpkgs }:
+  let
+    project_name = "easy-mode";
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
+      inherit system;
+      pkgs = nixpkgs.legacyPackages.${system};
+      hpkgs = pkgs.haskell.packages.ghc98;
+    });
+  in
+  rec {
+    packages = eachSystem ({hpkgs, ...}: {
+      default = hpkgs.callCabal2nix project_name ./. { };
+    });
+
+    devShells = eachSystem ({pkgs, hpkgs, system, ...}: {
+      default = pkgs.haskell.lib.addBuildTools packages.${system}.default
+        (with hpkgs; [ haskell-language-server cabal-install ]);
+    });
+  };
 }
